50b5cc44b6105f0ad3a19f57656cdc7774ccbb12
patalee narasinghe <patalee2001@gmail.com>
2024-08-28 19:33:47 +0530

    trying emotion classify model

--- temp_db_cons/db_init.py
+++ temp_db_cons/db_init.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 # Get the values from environment variables
 host = os.getenv('HOST')
+port = os.getenv('PORT')
 database = os.getenv('DATABASE')
 user = os.getenv('USER')
 password = os.getenv('PASSWORD')
@@ -14,9 +15,11 @@ password = os.getenv('PASSWORD')
 # Establish the connection
 conn = mysql.connector.connect(
     host=host,
+    port=port,
     user=user,
     password=password,
-    database=database
+    database=database,
+    ssl_disabled=False,
 )
 
 # Create a cursor object
